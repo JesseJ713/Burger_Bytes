d2b31729ce7f01b54a47111d4fd2c2d032edd0b5
JCJ <jesse.jackson.c@gmail.com>
2020-10-19 09:22:13 -0500

    Establishing route that will update data

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 let express = require("express");
 let burger = require("../models/burger");
 
@@ -16,5 +17,21 @@ router.get("/" , function (req, res) {
 router.post("/api/burgers", function (req, res) {
     burger.create(req.body.burger_name, function(result) {
         res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition: ", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status (404).end();
+        } else {
+            res.status(200).end();
+        };
     });
 });
