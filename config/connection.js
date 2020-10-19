f3c56bb86affeb885d6ed3fb8e02ceb4ee949b1f
JCJ <jesse.jackson.c@gmail.com>
2020-10-18 23:57:22 -0500

    Connecting to database

--- config/connection.js
+++ config/connection.js
@@ -8,3 +8,12 @@ var connection = mysql.createConnection({
     password: "",
     database: "burgers_db"
   });
+
+  // Make connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
