c268953a07cc997c738e1bc5801875350b95b063
JCJ <jesse.jackson.c@gmail.com>
2020-10-19 00:55:21 -0500

    Defining orm object with methods necessary to execute MySQL commands

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,28 @@
 let connection = require("./connection");
+
+let orm = {
+    selectAll: function(selectedTable, cb) {
+        let queryString = "SELECT * FROM ??";
+        connection.query(queryString, selectedTable, function(err, result) {
+          if (err) throw err;
+          cb(result);
+        });
+      },
+
+      insertOne: function(newBurger, cb) {
+        let queryString = "INSERT INTO burgers (burger_name) VALUE (?)";
+        connection.query(queryString, newBurger, function(err, result) {
+          if (err) throw err;
+          cb(result);
+        });
+      },
+
+      updateOne: function(uniqueId, cb) {
+        let queryString = "UPDATE burgers SET devoured = NOT devoured WHERE id = ?;
+        connection.query(queryString, uniqueId, function(err, result) {
+          if (err) throw err;
+          cb(result);
+        });
+      }
+
+    };
