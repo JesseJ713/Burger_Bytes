083597a6d319b0d1b9d47ba4cc5751868c27962f
JCJ <jesse.jackson.c@gmail.com>
2020-10-19 09:03:44 -0500

    Establishing route for main root directory

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,13 @@ let express = require("express");
 let burger = require("../models/burger");
 
 let router = express.Router();
+
+router.get("/" , function (req, res) {
+    burger.all(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log("hbsObject: ", hbsObject);
+        res.render("index", hbsObject);
+    });
+});
