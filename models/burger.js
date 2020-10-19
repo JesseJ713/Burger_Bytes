09d4235125b4d11eb2645d255f4eb1e35212cf9a
JCJ <jesse.jackson.c@gmail.com>
2020-10-19 08:10:08 -0500

    Created burger object that holds all methods that will communicate with database

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,21 @@
 let orm = require("../config/orm");
+
+let burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(newBurger, cb) {
+        orm.create("burgers", newBurger, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(uniqueId, cb) {
+        orm.update("burgers", uniqueId, function(res) {
+            cb(res);
+        });
+    }
+};
